5eb2ea863b712e90afcdc972fbc7f53122df59bb
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 09:26:31 +0200

    Created model for CampSpot table in database

---
+++ BookingSystem-ClassLibrary/Models/CampSpot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem_ClassLibrary.Models
+{
+    public class CampSpot
+    {
+        public int Id { get; set; }
+        public int CampSiteId { get; set; }
+        public int TypeId { get; set; }
+        public bool Electricity { get; set; } = false;
+        public int MaxPersonLimit { get; set; }
+
+        //Navigational properties
+        public CampSite? CampSite { get; set; }
+        public Type? Type { get; set; }
+        public ICollection<Booking>? Bookings { get; set; }
+    }
+}
