6f7a013008f75644d5ba4e9ae0711d121d8cefc1
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 09:33:12 +0200

    Created model for SpotType table in database

---
+++ BookingSystem-ClassLibrary/Models/SpotType.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingSystem_ClassLibrary.Models
+{
+    public class SpotType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        //Navigational Properties
+        public ICollection<CampSpot>? CampSpots { get; set; }
+    }
+}
