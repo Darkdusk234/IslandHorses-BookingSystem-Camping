db9bd6b1a8dc8a3c6e0eab0383c0dd117697b235
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 09:26:49 +0200

    Created model for Booking table in database

---
+++ BookingSystem-ClassLibrary/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace BookingSystem_ClassLibrary.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int CampSpotId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int NumberOfPeople { get; set; }
+
+        //Navigational Properties
+        public Customer? Customer { get; set; }
+        public CampSpot? CampSpot { get; set; }
+    }
+}
