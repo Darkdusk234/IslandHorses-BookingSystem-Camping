a6c2bb50558e6693116abf94ff71fde6f78d7d78
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2025-05-14 13:24:08 +0200

    Add BookingController with GetAllBookings endpoint
    The `<ItemGroup>` defining the `Controllers\` folder was removed
    from the `Camping-BookingSystem.csproj` file, simplifying the
    project structure.
    
    A new `BookingController` class was introduced in the
    `Camping_BookingSystem.Controllers` namespace. It is an API
    controller with `[ApiController]` and `[Route]` attributes,
    designed to handle HTTP requests.
    
    The controller depends on the `IBookingRepository` interface,
    injected via the constructor, adhering to the Dependency
    Injection pattern.
    
    A `GetAllBookings` asynchronous HTTP GET method was added to
    retrieve all bookings and return them with an `Ok` status.
    
    Necessary `using` directives were added to support the new
    functionality.

---
+++ Camping-BookingSystem/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Camping_BookingSystem.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Camping_BookingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public BookingController(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBookings()
+        {
+            var bookings = await _bookingRepository.GetAllAsync();
+            return Ok(bookings);
+        }
+
+
+    }
+}
