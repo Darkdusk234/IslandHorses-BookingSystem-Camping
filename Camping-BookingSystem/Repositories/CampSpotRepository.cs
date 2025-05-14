61e0bbb1f0230c3512b03e0ef895164038f08dc8
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 12:27:16 +0200

    Added context variable to class

--- Camping-BookingSystem/Repositories/CampSpotRepository.cs
+++ Camping-BookingSystem/Repositories/CampSpotRepository.cs
@@ -1,9 +1,16 @@
-﻿using BookingSystem_ClassLibrary.Models;
+﻿using BookingSystem_ClassLibrary.Data;
+using BookingSystem_ClassLibrary.Models;
 
 namespace Camping_BookingSystem.Repositories
 {
     public class CampSpotRepository : ICampSpotRepository
     {
+        private readonly CampingDbContext _context;
+
+        public CampSpotRepository(CampingDbContext context)
+        {
+            _context = context;
+        }
         public Task Create()
         {
             throw new NotImplementedException();
