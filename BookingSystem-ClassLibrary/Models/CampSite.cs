4da992d5487f6b67e03aa85d2a8f21d8c8110d49
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 09:26:12 +0200

    Created model for CampSite table in database

---
+++ BookingSystem-ClassLibrary/Models/CampSite.cs
@@ -0,0 +1,13 @@
+﻿namespace BookingSystem_ClassLibrary.Models
+{
+    public class CampSite
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Adress { get; set; } = null!;
+
+        //Navigational properties
+        public ICollection<CampSpot> CampSpots { get; set; } = new List<CampSpot>();
+    }
+}
