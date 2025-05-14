fbaee3750608caf4d90e673284450e6535783d65
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 12:25:12 +0200

    Created CampSpotRepository with implemented interface from ICampSpotRepository

---
+++ Camping-BookingSystem/Repositories/CampSpotRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookingSystem_ClassLibrary.Models;
+
+namespace Camping_BookingSystem.Repositories
+{
+    public class CampSpotRepository : ICampSpotRepository
+    {
+        public Task Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<CampSpot>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<CampSpot>> GetByCampSiteId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampSpot> GetCampSpotById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
