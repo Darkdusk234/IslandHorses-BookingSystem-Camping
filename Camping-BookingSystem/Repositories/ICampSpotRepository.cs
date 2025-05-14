c5857847c3a80c764482e0f0917a87553afa0478
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 12:23:38 +0200

    Created interface for CampRepository

---
+++ Camping-BookingSystem/Repositories/ICampSpotRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingSystem_ClassLibrary.Models;
+
+namespace Camping_BookingSystem.Repositories
+{
+    public interface ICampSpotRepository
+    {
+        public Task<ICollection<CampSpot>> GetAll();
+        public Task<CampSpot> GetCampSpotById();
+        public Task<ICollection<CampSpot>> GetByCampSiteId();
+        public Task Create();
+        public Task Update();
+        public Task Delete();
+    }
+}
