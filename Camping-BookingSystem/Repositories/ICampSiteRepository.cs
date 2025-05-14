7da28beb83dd4d6c3af1d7333303b6e5d0bb6e9d
Leon <leon.json57@gmail.com>
2025-05-14 11:31:39 +0200

    Created interface for campsite

---
+++ Camping-BookingSystem/Repositories/ICampSiteRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem_ClassLibrary.Models;
+
+namespace BookingSystem_ClassLibrary.Data
+{
+    public interface ICampSiteRepository
+    {
+        Task<IEnumerable<CampSite>> GetAllCampSitesAsync();
+        Task<CampSite?> GetCampSiteByIdAsync(int id);
+        Task<CampSite> AddCampSiteAsync(CampSite campSite);
+        Task<CampSite> UpdateCampSiteAsync(CampSite campSite);
+        Task<bool> DeleteCampSiteAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
