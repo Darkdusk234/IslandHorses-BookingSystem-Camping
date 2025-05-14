cb5bbaec761e56325cba4b0a6fa1681c7257bc6e
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-14 09:29:19 +0200

    Created model for Customer table in database

---
+++ BookingSystem-ClassLibrary/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace BookingSystem_ClassLibrary.Models
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string StreetAddress { get; set; } = null!;
+        public string ZipCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+        //Navigational Properties
+        public ICollection<Booking>? Bookings { get; set; }
+    }
+}
