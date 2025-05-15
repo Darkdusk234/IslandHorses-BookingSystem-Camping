4f5ca1e58972a24907ae55e843d1ed6d72edef22
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-15 13:05:02 +0200

    Added function to get database in memory for use in tests

--- Camping-BookingSystemTests/CampSpotRepositoryTests.cs
+++ Camping-BookingSystemTests/CampSpotRepositoryTests.cs
@@ -1,8 +1,19 @@
+using BookingSystem_ClassLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Camping_BookingSystemTests;
 
 [TestClass]
 public class CampSpotRepositoryTests
 {
+    private CampingDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<CampingDbContext>()
+            .UseInMemoryDatabase($"BookingTestDb_{Guid.NewGuid()}")
+            .Options;
+        return new CampingDbContext(options);
+    }
+
     [TestMethod]
     public void TestMethod1()
     {
