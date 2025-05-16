2e3d4ce5a50d3956594e6adc6782f0ccf39d818d
tantting <jennyann.hayward82@gmail.com>
2025-05-16 10:22:12 +0200

    Created a CustomerRepositoy.cs

---
+++ Camping-BookingSystem/Repositories/CustomerRepositoy.cs
@@ -0,0 +1,43 @@
+using BookingSystem_ClassLibrary.Data;
+using BookingSystem_ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Camping_BookingSystem.Repositories;
+
+public class CustomerRepositoy
+{
+    private readonly CampingDbContext _context;
+
+    public CustomerRepositoy(CampingDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync(); 
+    }
+
+    public async Task<Customer?> GetByIdAsync(int id)
+    {
+        return await _context.Customers.FindAsync(); 
+    }
+
+    public async Task AddAsync(Customer customer)
+    {
+        await _context.Customers.AddAsync(customer);
+        await _context.SaveChangesAsync(); 
+    }
+
+    public async Task UpdateAsync(Customer customer)
+    {
+        _context.Customers.Update(customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsynch(Customer customer)
+    {
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+    }
+}
