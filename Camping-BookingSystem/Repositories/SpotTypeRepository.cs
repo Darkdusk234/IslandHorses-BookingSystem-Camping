28b83ec4f51ee390666872de986f926f0a6a99e5
Conixen <leon.json57@gmail.com>
2025-05-19 13:50:04 +0200

    SpotTypeRepository

---
+++ Camping-BookingSystem/Repositories/SpotTypeRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookingSystem_ClassLibrary.Data;
+using BookingSystem_ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Camping_BookingSystem.Repositories
+{
+    public class SpotTypeRepository
+    {
+        private readonly CampingDbContext _context;
+        public SpotTypeRepository(CampingDbContext context)
+        {
+            _context = context;
+        }
+        // Default methods for CRUD operations
+        public async Task<IEnumerable<SpotType>> GetAllAsync()
+        {
+            return await _context.SpotTypes.ToListAsync();
+        }
+        public async Task<SpotType?> GetByIdAsync(int id)
+        {
+            return await _context.SpotTypes.FindAsync(id);
+        }
+        public async Task AddAsync(SpotType spotType)
+        {
+            await _context.SpotTypes.AddAsync(spotType);
+            await SaveAsync();
+        }
+        public void Update(SpotType spotType)
+        {
+            _context.SpotTypes.Update(spotType);
+            SaveAsync();
+        }
+        public void Delete(SpotType spotType)
+        {
+            _context.SpotTypes.Remove(spotType);
+            SaveAsync();
+        }
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
