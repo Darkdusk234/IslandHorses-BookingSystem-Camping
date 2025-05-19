de2f8cf0d6bbb34fa1f65e6a59e184c1755823bf
Conixen <leon.json57@gmail.com>
2025-05-19 13:46:51 +0200

    ISpotTypeRepository

---
+++ Camping-BookingSystem/Repositories/ISpotTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookingSystem_ClassLibrary.Models;
+
+namespace Camping_BookingSystem.Repositories
+{
+    public class ISpotTypeRepository
+    {
+        //Default methods for CRUD operations
+        Task<IEnumerable<SpotType>> GetAllAsync();
+        Task<SpotType?> GetByIdAsync(int id);
+        Task AddAsync(SpotType spotType);
+        void Update(SpotType spotType);
+        void Delete(SpotType spotType);
+        Task SaveAsync();
+
+    }
+}
