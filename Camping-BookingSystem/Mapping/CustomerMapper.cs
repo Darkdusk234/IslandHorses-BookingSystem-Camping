d0ea01146087a16abdff2b1161933b3709b39571
tantting <jennyann.hayward82@gmail.com>
2025-05-21 15:18:25 +0200

    Added a ToCustomerResponse-mapper

--- Camping-BookingSystem/Mapping/CustomerMapper.cs
+++ Camping-BookingSystem/Mapping/CustomerMapper.cs
@@ -1,7 +1,23 @@
+using BookingSystem_ClassLibrary.Models;
+using BookingSystem_ClassLibrary.Models.DTOs.BookingDTOs;
+using BookingSystem_ClassLibrary.Models.DTOs.CustomerDTOs;
+
 namespace Camping_BookingSystem.Mapping;
 
 //Borrow mapper structure from BookingMapper(by Fredrik) for a more consistent approach. 
-public class CustomerMapper
+public static class CustomerMapper
 {
-    
+    public static CustomerResponse ToCustomerResponse(this Customer customer)
+    {
+        return new CustomerResponse()
+        {
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Email = customer.Email,
+            PhoneNumber = customer.PhoneNumber,
+            StreetAddress = customer.StreetAddress,
+            ZipCode = customer.ZipCode,
+            City = customer.City
+        };
+    }
 }
