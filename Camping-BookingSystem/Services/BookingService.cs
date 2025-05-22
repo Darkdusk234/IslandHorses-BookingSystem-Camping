ce1ef3213e5a867fe6d51bb72e4cf42ab9d83400
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2025-05-22 22:18:37 +0200

    Added some comments to BookingService.cs
    There is still some questions that i added to the comments.

--- Camping-BookingSystem/Services/BookingService.cs
+++ Camping-BookingSystem/Services/BookingService.cs
@@ -13,6 +13,7 @@ namespace Camping_BookingSystem.Services
         {
             _bookingRepository = bookingRepository;
         }
+
         // Method to cancel a booking (Guest)
         public async Task<(bool Success, string? ErrorMEssage)> CancelBookingAsync(int bookingId)
         {
@@ -37,14 +38,14 @@ namespace Camping_BookingSystem.Services
             return (true, null);
 
         }
-
+        // Method to create a booking (Guest)
         public async Task<Booking> CreateBookingAsync(Booking booking)
         {
             await _bookingRepository.AddAsync(booking);
             await _bookingRepository.SaveAsync();
             return booking;
         }
-
+        // Method to create a booking and add a customer (Receptionist)
         public async Task<BookingDetailsResponse> CreateBookingWithCustomerAsync(CreateBookingAndCustomer request)
         {
             
@@ -74,6 +75,7 @@ namespace Camping_BookingSystem.Services
             await _bookingRepository.AddAsync(booking);
             await _bookingRepository.SaveAsync();
             // Get the full booking with customer details (made this so EF has time to present information in response body)
+            // Still not getting the expected result, PETTER?! JOHAN?! Hjälp!
             var fullBooking = await _bookingRepository.GetByIdAsync(booking.Id);
 
             var response = fullBooking.ToBookingDetailsResponse();
@@ -82,7 +84,7 @@ namespace Camping_BookingSystem.Services
 
             return response;
         }
-
+        // Method to delete a booking (Camp Owner)
         public async Task DeleteBookingAsync(int id)
         {
             var booking = await _bookingRepository.GetByIdAsync(id);
@@ -92,7 +94,8 @@ namespace Camping_BookingSystem.Services
                 await _bookingRepository.SaveAsync();
             }
         }
-
+        // Method to get all bookings (Camp Owner)
+        // Detta blir ju för hela databasen? Borde kanske ändras så det är CampSiteId specific?
         public async Task<IEnumerable<BookingDetailsResponse>> GetAllBookingsAsync()
         {
             var bookings = await _bookingRepository.GetAllAsync();
@@ -110,7 +113,7 @@ namespace Camping_BookingSystem.Services
         {
             return await _bookingRepository.GetByIdAsync(id);
         }
-
+        // Method to get all bookings by customer id (Receptionist)
         public async Task<IEnumerable<BookingDetailsResponse>> GetBookingsByCustomerIdAsync(int customerId)
         {
             var bookings = await _bookingRepository.GetBookingsByCustomerIdAsync(customerId);
@@ -137,7 +140,7 @@ namespace Camping_BookingSystem.Services
                 b.EndDate >= startDate &&
                 b.StartDate <= endDate);
         }
-
+        // Method to update the booking add-ons (Wifi and Parking) (Guest)
         public async Task<(bool Success, string? ErrorMessage)> UpdateBookingAddOnsAsync(int bookingId, UpdateAddonsRequest request)
         {
             var booking = await _bookingRepository.GetByIdAsync(bookingId);
@@ -159,7 +162,7 @@ namespace Camping_BookingSystem.Services
 
             return (true, null);
         }
-
+        // Method to update the booking (Receptionist)
         public async Task<(bool Success, string? ErrorMessage)> UpdateBookingAsyn(int bookingId, UpdateBookingRequest request)
         {
             var booking = await _bookingRepository.GetByIdAsync(bookingId);
@@ -184,12 +187,12 @@ namespace Camping_BookingSystem.Services
             return (true, null);
 
         }
-
+        // Method to calculate the total number of nights for a booking
         private int CalculateTotalNights(Booking booking)
         {
             return (booking.EndDate - booking.StartDate).Days;
         }
-
+        // Method to calculate the total price for a booking including add-ons
         private decimal CalculateTotalPrice(Booking booking)
         {
             if (booking.CampSpot?.SpotType == null)
