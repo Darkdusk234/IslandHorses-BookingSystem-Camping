5211a8c9b8ed39fea5e627c6573426664709494e
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-22 10:54:50 +0200

    Added implementaion of DeleteCampSpotAsync method

--- Camping-BookingSystem/Services/CampSpotService.cs
+++ Camping-BookingSystem/Services/CampSpotService.cs
@@ -1,17 +1,29 @@
 ﻿using BookingSystem_ClassLibrary.Models;
+using Camping_BookingSystem.Repositories;
 
 namespace Camping_BookingSystem.Services
 {
     public class CampSpotService : ICampSpotService
     {
+        private readonly ICampSpotRepository _campSpotRepository;
+
+        public CampSpotService(ICampSpotRepository campSpotRepository)
+        {
+            _campSpotRepository = campSpotRepository;
+        }
+
         public Task CreateCampSpotAsync(CampSpot campSpot)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteCampSpotAsync(int id)
+        public async Task DeleteCampSpotAsync(int id)
         {
-            throw new NotImplementedException();
+            var campSpot = await _campSpotRepository.GetCampSpotById(id);
+            if(campSpot != null)
+            {
+                await _campSpotRepository.Delete(campSpot);
+            }
         }
 
         public Task<IEnumerable<CampSpot>> GetAllCampSpotsAsync()
