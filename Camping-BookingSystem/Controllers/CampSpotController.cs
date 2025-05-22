457e2a6b4cca4da3fb75b83173f095a71f621c9a
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-22 14:30:23 +0200

    Created CampSpot Controller

---
+++ Camping-BookingSystem/Controllers/CampSpotController.cs
@@ -0,0 +1,18 @@
+﻿using Camping_BookingSystem.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Camping_BookingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampSpotController : ControllerBase
+    {
+        private readonly ICampSpotService _campSpotService;
+
+        public CampSpotController(ICampSpotService campSpotService)
+        {
+            _campSpotService = campSpotService;
+        }
+    }
+}
