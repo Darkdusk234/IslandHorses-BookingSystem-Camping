a172b0f593f18636b1d36b9a264267925a00047d
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-22 10:31:17 +0200

    Created CampSpotService and implemented the interface for it

---
+++ Camping-BookingSystem/Services/CampSpotService.cs
@@ -0,0 +1,37 @@
+﻿using BookingSystem_ClassLibrary.Models;
+
+namespace Camping_BookingSystem.Services
+{
+    public class CampSpotService : ICampSpotService
+    {
+        public Task CreateCampSpotAsync(CampSpot campSpot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCampSpotAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CampSpot>> GetAllCampSpotsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CampSpot>> GetCampSpotByCampSiteIdAsync(int campSiteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampSpot> GetCampSpotByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCampSpotAsync(CampSpot campSpot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
