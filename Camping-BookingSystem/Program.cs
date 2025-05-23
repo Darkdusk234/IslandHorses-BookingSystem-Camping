f62ea657fb469da19c04670950734e735401794b
darkdusk234 <johan.svensson.01@hotmail.com>
2025-05-23 11:47:08 +0200

    Added service for campspot service and repository in program file

--- Camping-BookingSystem/Program.cs
+++ Camping-BookingSystem/Program.cs
@@ -14,6 +14,8 @@ namespace Camping_BookingSystem
             // Add services to the container.
             builder.Services.AddScoped<IBookingService, BookingService>();
             builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+            builder.Services.AddScoped<ICampSpotService, CampSpotService>();
+            builder.Services.AddScoped<ICampSpotRepository, CampSpotRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
