9a2bc217ce8ef0b6bc6edfc9d8516f7f167b2fd5
Leon <leon.json57@gmail.com>
2025-05-26 12:10:39 +0200

    Leon spot type patterns (#66)
    * Created model for camping site table
    
    * SpotTypeDetailsResponse
    
    * CreateSpotTypeRequest
    
    * UpdateSpotTypeRequest
    
    * SpotTypeMapper
    
    * ISpotTypeRepository
    
    * SpotTypeRepository
    
    * IRepository interface not class
    
    * SpotType Service och IService
    
    * SpotTypeController
    
    * renamed,  tonewspottype, and removed async repository
    
    * add saveasync removed
    
    ---------
    
    Co-authored-by: darkdusk234 <johan.svensson.01@hotmail.com>

---
+++ Camping-BookingSystem/Mapping/SpotTypeMapper.cs
@@ -0,0 +1,36 @@
+﻿using BookingSystem_ClassLibrary.Models;
+using BookingSystem_ClassLibrary.Models.DTOs.SpotTypeDTOs;
+
+namespace Camping_BookingSystem.Mapping
+{
+    public static class SpotTypeMapper
+    {
+        public static SpotType ToNewSpotType(this CreateSpotTypeRequest dto)
+        {
+            return new SpotType
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Price = dto.Price
+            };
+        }
+        public static SpotType ToUpdatedSpotType(this UpdateSpotTypeRequest dto)
+        {
+            return new SpotType
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Price = dto.Price
+            };
+        }
+        public static SpotTypeDetailsResponse ToSpotTypeDetailsResponse(this SpotType spotType)
+        {
+            return new SpotTypeDetailsResponse
+            {
+                Id = spotType.Id,
+                Name = spotType.Name,
+                Price = spotType.Price
+            };
+        }
+    }
+}
