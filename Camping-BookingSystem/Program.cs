199887884ba0594a51e93b5f4539c5c33b69ba3d
tantting <jennyann.hayward82@gmail.com>
2025-05-27 10:00:46 +0200

    Added a "ignore cycles service"

--- Camping-BookingSystem/Program.cs
+++ Camping-BookingSystem/Program.cs
@@ -1,4 +1,5 @@
 
+using System.Text.Json.Serialization;
 using Camping_BookingSystem.Repositories;
 using Camping_BookingSystem.Services;
 using Microsoft.EntityFrameworkCore;
@@ -19,7 +20,10 @@ namespace Camping_BookingSystem
             builder.Services.AddScoped<ICampSpotService, CampSpotService>();
             builder.Services.AddScoped<ICampSpotRepository, CampSpotRepository>();
 
-
+            builder.Services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = 
+                    ReferenceHandler.IgnoreCycles);
+            
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
