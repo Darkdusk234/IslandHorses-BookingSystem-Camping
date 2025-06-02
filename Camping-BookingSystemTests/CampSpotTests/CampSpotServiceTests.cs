eadc29ffef0c8aa00bfc0a5e89b2b53e4ef60173
darkdusk234 <johan.svensson.01@hotmail.com>
2025-06-02 10:07:17 +0200

    Created MOQ test for AddCampSpotAsync

--- Camping-BookingSystemTests/CampSpotTests/CampSpotServiceTests.cs
+++ Camping-BookingSystemTests/CampSpotTests/CampSpotServiceTests.cs
@@ -1,10 +1,47 @@
+using BookingSystem_ClassLibrary.Data;
+using BookingSystem_ClassLibrary.Models;
+using Camping_BookingSystem.Repositories;
+using Camping_BookingSystem.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
 namespace Camping_BookingSystemTests;
 
 [TestClass]
 public class CampSpotServiceTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    private CampingDbContext _context;
+    private CampSpotService _campSpotService;
+    private Mock<ICampSpotRepository> _campSpotRepoMock;
+    private Mock<ICampSiteRepository> _campSiteRepoMock;
+
+    [TestInitialize]
+    public void Initialize()
     {
+        _campSpotRepoMock = new Mock<ICampSpotRepository>();
+        _campSiteRepoMock = new Mock<ICampSiteRepository>();
+
+        _campSpotService = new CampSpotService(_campSpotRepoMock.Object, _campSiteRepoMock.Object);
+    }
+
+    [TestMethod]
+    public void AddCampSpotAsync_InputtingValidCampSpot_CampSpotThatWasAdded()
+    {
+        
+        var campSpot = new CampSpot
+        {
+            Id = 1,
+            CampSiteId = 1,
+            TypeId = 1,
+            Electricity = true
+        };
+
+        _campSpotRepoMock.Setup(m => m.Create(It.IsAny<CampSpot>())).Returns(Task.CompletedTask);
+
+        var result = _campSpotService.AddCampSpotAsync(campSpot).Result;
+
+        Assert.AreEqual(JsonConvert.SerializeObject(campSpot), JsonConvert.SerializeObject(result));
+        _campSpotRepoMock.Verify(repo => repo.Create(It.IsAny<CampSpot>()), Times.Once);
     }
 }
