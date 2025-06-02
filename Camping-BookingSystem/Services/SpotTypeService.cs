b18023ae76b024d75a22d1854b5a877c0e6a702d
Leon <leon.json57@gmail.com>
2025-06-02 10:06:45 +0200

    comments

--- Camping-BookingSystem/Services/SpotTypeService.cs
+++ Camping-BookingSystem/Services/SpotTypeService.cs
@@ -20,22 +20,22 @@ namespace Camping_BookingSystem.Services
         }
         public async Task<SpotType> CreateSpotTypeAsync(SpotType spotType)
         {
-            if (spotType == null)
+            if (spotType == null)   // check if spotType is null
             {
                 throw new ArgumentNullException(nameof(spotType));
             }
 
-            if (string.IsNullOrWhiteSpace(spotType.Name))
+            if (string.IsNullOrWhiteSpace(spotType.Name))   // check if name is empty or whitespace
             {
                 throw new ArgumentException("Name cannot be empty", nameof(spotType));
             }
 
-            if (spotType.Price < 0)
+            if (spotType.Price < 0)     // check if price is negative
             {
                 throw new ArgumentException("Price cannot be negative", nameof(spotType));
             }
 
-            if (spotType.MaxPersonLimit <= 0)
+            if (spotType.MaxPersonLimit <= 0)   // check if MaxPersonLimit is less than or equal to 0
             {
                 throw new ArgumentException("MaxPersonLimit must be greater than 0", nameof(spotType));
             }
