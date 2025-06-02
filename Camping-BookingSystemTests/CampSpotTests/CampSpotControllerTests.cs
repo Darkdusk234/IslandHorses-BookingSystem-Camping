3ba7207214b5a069f30498a833e6a67c0eac0a13
darkdusk234 <johan.svensson.01@hotmail.com>
2025-06-02 14:48:25 +0200

    Added test for GetAllCampSpots when getting all camp spots

--- Camping-BookingSystemTests/CampSpotTests/CampSpotControllerTests.cs
+++ Camping-BookingSystemTests/CampSpotTests/CampSpotControllerTests.cs
@@ -1,6 +1,9 @@
+using BookingSystem_ClassLibrary.Models;
 using Camping_BookingSystem.Controllers;
 using Camping_BookingSystem.Services;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Newtonsoft.Json;
 
 namespace Camping_BookingSystemTests;
 
@@ -19,7 +22,36 @@ public class CampSpotControllerTests
     }
 
     [TestMethod]
-    public void TestMethod1()
+    public async Task GetAllCampSpots_WhenGettingAllCampSpots_ListOfAllCampSpotsAsync()
     {
+        var campSpot1 = new CampSpot
+        {
+            Id = 1,
+            CampSiteId = 1,
+            TypeId = 1,
+            Electricity = false
+        };
+        var campSpot2 = new CampSpot
+        {
+            Id = 1,
+            CampSiteId = 1,
+            TypeId = 1,
+            Electricity = false
+        };
+        var campSpot3 = new CampSpot
+        {
+            Id = 1,
+            CampSiteId = 1,
+            TypeId = 1,
+            Electricity = false
+        };
+        var list = new List<CampSpot> { campSpot1, campSpot2, campSpot3 };
+        _campSpotServiceMock.Setup(m => m.GetAllCampSpotsAsync()).ReturnsAsync(list);
+        OkObjectResult expected = new OkObjectResult(list);
+
+        var actual = await _campSpotController.GetAllCampSpots();
+
+        Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        _campSpotServiceMock.Verify(repo => repo.GetAllCampSpotsAsync(), Times.Once());
     }
 }
